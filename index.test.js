fd7e1c443a14ff6996fe17a9778c6f56281aa0da
John O Raab <125783786+jrsr5599@users.noreply.github.com>
2023-07-27 10:31:18 -0700

    Add files via upload

--- index.test.js
+++ index.test.js
@@ -10,3 +10,23 @@ describe('Circle', () => {
         expect(actual).toEqual(testcircle)
       });
 })
+
+describe('Triangle', () => {
+    it('should create a blue triangle', () => {
+       const testtriangle = "<polygon points=\"150\", \"18\", \"244\", \"182\", \"56\", \"182\" fill=\"Blue\" />"
+        const newtriangle = new Triangle();
+        newtriangle.setColor("Blue")
+        const actual = newtriangle.render() 
+        expect(actual).toEqual(testtriangle)
+      });
+})
+
+describe('Square', () => {
+    it('should create a blue square', () => {
+       const testsquare = "<rect x=\"73\" y=\"40\" width=\"160\" height=\"160\" fill=\"Blue\" />"
+        const newsquare = new Square();
+        newsquare.setColor("Blue")
+        const actual = newsquare.render() 
+        expect(actual).toEqual(testsquare)
+      });
+})
